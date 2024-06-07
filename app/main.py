d1644502acbf3adacc8fde0535294072ea3807f1
Ahmed <shiekhahmed135@gmail.com>
2024-06-08 02:25:06 +0500

    flow completed

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
+import google.generativeai as genai
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+import os
+import pyperclip
+
+# Load the environment variables
+from dotenv import load_dotenv
+load_dotenv()
 
 
 class TelegramAutomation():
@@ -24,23 +32,22 @@ class TelegramAutomation():
 
         # Wait for the user to manually log in
         print("Please log in to Telegram Web manually within 60 seconds.")
-        time.sleep(60)
+        time.sleep(30)
+        print("30 seconds remaining...")
+        time.sleep(30)
 
         # Check if the login was successful
         try:
-            _ = self.driver.find_element(
-                By.XPATH, '//*[contains(@class, "input-search-input")]')
+            print("Checking if login was successful...")
+            _ = self.driver.find_element(By.CLASS_NAME, "input-search-input")
             print("Login successful!")
-        except Exception:
-            print("Login failed. Please check your credentials and try again.")
-            self.driver.quit()
-            exit()
+        except Exception as e:
+            print("Login failed because: ", str(e))
+            self.close()
 
     def go_to_source_group(self, url):
         # Now you can proceed to source group
         self.go_to_group(url)
-        print("Waiting for 10 seconds to load the group...")
-        time.sleep(10)
 
         # checking for new message
         while True:
@@ -50,28 +57,59 @@ class TelegramAutomation():
                 last_message = messages[-1].text
                 print("Last message:", last_message)
                 if self.check_for_new_meessage(last_message):
-                    print("New message found!", last_message)
+                    if "facebook" not in last_message.lower():
+                        self.message = last_message
+                        print("New message found!", last_message)
                     break
             print("Waiting for 10 seconds to check for new messages...")
             time.sleep(10)
 
     def check_for_new_meessage(self, msg):
         if msg != self.message:
-            self.message = msg
             return True
         return False
 
     def go_to_group(self, url):
+        print("Going to the group: ", url)
         self.driver.get(url)
+        print("Waiting for 10 seconds to load the group...")
+        time.sleep(10)
 
-    def format_message(self, message):
-        return message
+    def format_message(self):
+        """
+        Covering 2 scenarios for now:
+        1. Format the msg with GEMINI API
+        2. Send as it is
+        """
+        # first trim the date from message
+        msg = self.message
+        msg = msg.rsplit("\n", 2)[0]
+        print("Trimmed message:", msg)
+
+        # now try with GEMINI API else send as it is
+        try:
+            genai.configure(api_key=os.getenv('API_KEY'))
+            model = genai.GenerativeModel(model_name='gemini-1.5-flash')
+            response = model.generate_content(
+                f"""Please paraphrase this message, do make sure to use emojis
+                but less and dont change the coin name: {msg}""")
+            resp = response.text
+            print(resp)
+            return resp
+        except Exception as e:
+            print("GEMINI API didn't work because:", str(e))
+            print("Sending the message as it is...")
+            return msg
 
     def send_message(self, message):
-        message_box = self.driver.find_element(
-            By.XPATH, '//*[contains(@class, "composer_rich_textarea")]')
-        message_box.send_keys(message)
-        message_box.send_keys(Keys.RETURN)
+        pyperclip.copy(message)
+        act = ActionChains(self.driver)
+        act.key_down(Keys.CONTROL).send_keys(
+            "v").key_up(Keys.CONTROL).perform()
+        # message_box = self.driver.find_element(
+        #     By.CLASS_NAME, "input-message-input")
+        # message_box.send_keys(message)
+        # message_box.send_keys(Keys.RETURN)
 
     def close(self):
         print("Closing the browser in 5 seconds...")
@@ -86,10 +124,12 @@ if __name__ == "__main__":
         # 1. Login
         cond = bot.login()
         # 2. Go to the source group
-        bot.go_to_source_group('')
-        # formatted_message = self.format_message(last_message)
-        # self.go_to_group()
-        # self.send_message(formatted_message)
+        bot.go_to_source_group(os.getenv('SOURCE_GROUP_URL'))
+        formatted_message = bot.format_message()
+        bot.go_to_group(os.getenv("TARGET_GROUP_URL"))
+        print("Sending the message:", formatted_message)
+        bot.send_message(formatted_message)
+        print("Message sent successfully!")
         bot.close()
     except Exception as e:
         print("An error occurred:", str(e))
